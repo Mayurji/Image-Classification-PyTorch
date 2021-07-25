4a712372a0720c9578422b641cebca13ed854189
Mayur <mayur87545@gmail.com>
2021-07-25 16:07:03 +0530

    Create Xception.py

---
+++ Xception.py
@@ -0,0 +1,160 @@
+"""
+Xception
+
+The network uses a modified version of Depthwise Seperable Convolution. It combines
+ideas from MobileNetV1 like depthwise seperable conv and from InceptionV3, the order 
+of the layers like conv1x1 and then spatial kernels.
+
+In modified Depthwise Seperable Convolution network, the order of operation is changed
+by keeping Conv1x1 and then the spatial convolutional kernel. And the other difference
+is the absence of Non-Linear activation function. And with inclusion of residual 
+connections impacts the performs of Xception widely.
+
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+
+class SeparableConv(nn.Module):
+    def __init__(self, input_channel, output_channel, kernel_size=1, stride=1, padding=0, dilation=1, bias=False):
+        super().__init__()
+        self.dwc = nn.Sequential(
+            nn.Conv2d(input_channel, input_channel, kernel_size, stride, padding, dilation, groups=input_channel, bias=bias),
+            nn.Conv2d(input_channel, output_channel, 1, 1, 0, 1, 1, bias=bias)
+        )
+    
+    def forward(self, X):
+        return self.dwc(X)
+
+class Block(nn.Module):
+    def __init__(self, input_channel, out_channel, reps, strides=1, relu=True, grow_first=True):
+        super().__init__()
+        if out_channel != input_channel or strides!=1:
+            self.skipConnection = nn.Sequential(
+                nn.Conv2d(input_channel, out_channel, 1, stride=strides, bias=False),
+                nn.BatchNorm2d(out_channel)
+            )
+        else:
+            self.skipConnection = None
+        self.relu = nn.ReLU(inplace=True)
+        rep = []
+
+        filters = input_channel
+        if grow_first:
+            rep.append(self.relu)
+            rep.append(SeparableConv(input_channel, out_channel, 3, stride=1, padding=1, bias=False))
+            rep.append(nn.BatchNorm2d(out_channel))
+            filters = out_channel
+        
+        for _ in range(reps-1):
+            rep.append(self.relu)
+            rep.append(SeparableConv(filters, filters, 3, stride=1, padding=1, bias=False))
+            rep.append(nn.BatchNorm2d(filters))
+        
+        if not grow_first:
+            rep.append(self.relu)
+            rep.append(SeparableConv(input_channel, out_channel, 3, stride=1, padding=1, bias=False))
+            rep.append(nn.BatchNorm2d(out_channel))
+
+        if not relu:
+            rep = rep[1:]
+        else:
+            rep[0] = nn.ReLU(inplace=False)
+
+        if strides != 1:
+            rep.append(nn.MaxPool2d(3, strides, 1))
+
+        self.rep = nn.Sequential(*rep)
+
+    def forward(self, input):
+        X = self.rep(input)
+
+        if self.skipConnection:
+            skip = self.skipConnection(input)
+        else:
+            skip = input
+        
+        X += skip
+        return X
+
+class Xception(nn.Module):
+    def __init__(self, input_channel, n_classes):
+        super().__init__()
+        self.n_classes = n_classes
+        self.relu = nn.ReLU(inplace=True)
+
+        self.initBlock = nn.Sequential(
+            nn.Conv2d(input_channel, 32, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(32, 64, kernel_size = 3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+
+        self.block1=Block(64,128,2,2,relu=False,grow_first=True)
+        self.block2=Block(128,256,2,2,relu=True,grow_first=True)
+        self.block3=Block(256,728,2,2,relu=True,grow_first=True)
+
+        self.block4=Block(728,728,3,1,relu=True,grow_first=True)
+        self.block5=Block(728,728,3,1,relu=True,grow_first=True)
+        self.block6=Block(728,728,3,1,relu=True,grow_first=True)
+        self.block7=Block(728,728,3,1,relu=True,grow_first=True)
+
+        self.block8=Block(728,728,3,1,relu=True,grow_first=True)
+        self.block9=Block(728,728,3,1,relu=True,grow_first=True)
+        self.block10=Block(728,728,3,1,relu=True,grow_first=True)
+        self.block11=Block(728,728,3,1,relu=True,grow_first=True)
+
+        self.block12=Block(728,1024,2,2,relu=True,grow_first=False)
+
+        self.conv3 = SeparableConv(1024,1536,3,1,1)
+        self.bn3 = nn.BatchNorm2d(1536)
+
+        #do relu here
+        self.conv4 = SeparableConv(1536,2048,3,1,1)
+        self.bn4 = nn.BatchNorm2d(2048)
+
+        self.fc = nn.Linear(2048, self.n_classes)
+
+        #weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.initBlock(x)
+        x = self.block1(x)
+        
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.block6(x)
+        x = self.block7(x)
+        x = self.block8(x)
+        x = self.block9(x)
+        x = self.block10(x)
+        x = self.block11(x)
+        x = self.block12(x)
+        
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+        
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = self.relu(x)
+
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
