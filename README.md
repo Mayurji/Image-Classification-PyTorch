a6186f98e0f0be5bfbe2b79d73b6c982c45a48f6
mayurji <mayur87545@gmail.com>
2021-08-01 02:11:35 +0530

    ReadMe Changes

--- README.md
+++ README.md
@@ -14,22 +14,22 @@ Base Config: {epochs: 10, lr: 0.001, batch_size: 128, img_resolution: 224, optim
 
 I've noticed that Google Colab has 12GB GPU while Kaggle has 16 GB GPU. So in worst case scenario, I've reduced the batch size in accordance to fit the Kaggle GPU. Just to mention, I use RTX2070 8GB.
 
-|CNN Based    | Accuracy | Parameters     | Configuration | Working |
-| :---        |    :----:   | :----:       | :----:       | ---: |
+|CNN Based    | Accuracy | Parameters     | FLOPS | Configuration | Working |
+| :---        |    :----:   | :----:       | :----:       | :----:       | ---: |
 | [AlexNet](https://papers.nips.cc/paper/2012/hash/c399862d3b9d6b76c8436e924a68c45b-Abstract.html")| 71.27 | 58.32M || Yes |
 | [VGGNet](https://arxiv.org/abs/1409.1556)   | 75.21 | 128.81M || Yes |
 | [Network In Network](https://arxiv.org/abs/1312.4400) | 63.46 | 2.02M || Yes |
 | [ResNet](https://arxiv.org/abs/1512.03385)  | 80.51 | 11.18M || Yes |
 | [DenseNet](https://arxiv.org/abs/1608.06993)   | - | - | High Compute Req. | - |
 | [MobileNetV1](https://arxiv.org/abs/1704.04861)   | 81.35 | 3.22M || Yes |
-| [MobileNetV2](https://arxiv.org/abs/1801.04381)   | 80.74 | 2.24M ||
+| [MobileNetV2](https://arxiv.org/abs/1801.04381)   | 80.74 | 2.24M || Yes |
 | [GoogLeNet](https://arxiv.org/abs/1409.4842)   | 73.62 | 5.98M || Yes |
 | [InceptionV3](https://arxiv.org/abs/1512.00567)   | - | - | High Compute Req. | - |
 | [Darknet-53](https://arxiv.org/pdf/1804.02767.pdf)   | - | - | High Compute Req. | - |
 | [Xception](https://arxiv.org/abs/1610.02357)   | 85.9 | 20.83M | Batch_Size = 96 | Yes |
-| [ResNeXt](https://arxiv.org/abs/1611.05431)   | - |
+| [ResNeXt](https://arxiv.org/abs/1611.05431)   | - | | High Compute Req. | |
 | [SENet](https://arxiv.org/abs/1709.01507)   | 83.55 | 11.23M | | Yes |
-| [SqueezeNet](https://arxiv.org/abs/1602.07360v4)   | - | 0.73M ||
+| [SqueezeNet](https://arxiv.org/abs/1602.07360v4)   | - | 0.73M | Batch_Size = 64 |
 | [ShuffleNet](https://arxiv.org/abs/1707.01083)   | | | Batch_Size = 32 | Yes |
 | [EfficientNet](https://arxiv.org/abs/1905.11946)   | - |4.02M||
 | Transformer Based |
@@ -37,6 +37,9 @@ I've noticed that Google Colab has 12GB GPU while Kaggle has 16 GB GPU. So in wo
 | MLP Based |
 | [MLP-Mixer](https://arxiv.org/abs/2105.01601) | 64.98 | 13.63M | |
 | [ResMLP](https://arxiv.org/abs/2105.03404)| 70.17 | 14.97M |
+
+Note: I've marked few cells are high compute required because even with batch_size = 8, the kaggle compute was not enough, 
+if you have any work around or else, if something is missing please do share.
 
 ### Content
 
--- ResNeXt.py
+++ ResNeXt.py
@@ -1,76 +1,77 @@
-# """
-# ResNeXt is a simple, highly modularized network architecture for image classification. The
-# network is constructed by repeating a building block that aggregates a set of transformations 
-# with the same topology. The simple design results in a homogeneous, multi-branch architecture 
-# that has only a few hyper-parameters to set. This strategy exposes a new dimension, which is 
-# referred as “cardinality” (the size of the set of transformations), as an essential factor in 
-# addition to the dimensions of depth and width.
+"""
+ResNeXt is a simple, highly modularized network architecture for image classification. The
+network is constructed by repeating a building block that aggregates a set of transformations 
+with the same topology. The simple design results in a homogeneous, multi-branch architecture 
+that has only a few hyper-parameters to set. This strategy exposes a new dimension, which is 
+referred as “cardinality” (the size of the set of transformations), as an essential factor in 
+addition to the dimensions of depth and width.
 
-# We can think of cardinality as the set of separate conv block representing same complexity as 
-# when those blocks are combined together to make a single block.
+We can think of cardinality as the set of separate conv block representing same complexity as 
+when those blocks are combined together to make a single block.
 
-# Blog: https://towardsdatascience.com/review-resnext-1st-runner-up-of-ilsvrc-2016-image-classification-15d7f17b42ac
+Blog: https://towardsdatascience.com/review-resnext-1st-runner-up-of-ilsvrc-2016-image-classification-15d7f17b42ac
 
-# #### Citation ####
+#### Citation ####
 
-# PyTorch Code: https://github.com/prlz77/ResNeXt.pytorch
+PyTorch Code: https://github.com/prlz77/ResNeXt.pytorch
 
-# @article{Xie2016,
-#   title={Aggregated Residual Transformations for Deep Neural Networks},
-#   author={Saining Xie and Ross Girshick and Piotr Dollár and Zhuowen Tu and Kaiming He},
-#   journal={arXiv preprint arXiv:1611.05431},
-#   year={2016}
-# }
+@article{Xie2016,
+  title={Aggregated Residual Transformations for Deep Neural Networks},
+  author={Saining Xie and Ross Girshick and Piotr Dollár and Zhuowen Tu and Kaiming He},
+  journal={arXiv preprint arXiv:1611.05431},
+  year={2016}
+}
 
-import torch
+"""
+
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class Block(nn.Module):
-  expansion = 2  
-  """
-  '''Grouped convolution block.'''
-  groups: Integer 
-  It controls the connections between inputs and outputs. in_channels and out_channels must 
-  both be divisible by groups. For example,
+    expansion = 2  
+    """
+    '''Grouped convolution block.'''
+    groups: Integer 
+    It controls the connections between inputs and outputs. in_channels and out_channels must 
+    both be divisible by groups. For example,
 
-  At groups=1, all inputs are convolved to all outputs.
-  At groups=2, the operation becomes equivalent to having two conv layers side by side, each 
-  seeing half the input channels and producing half the output channels, and both subsequently 
-  concatenated.
-  At groups= in_channels, each input channel is convolved with its own set of filters 
-  (of size (out_channels/in_channels))
+    At groups=1, all inputs are convolved to all outputs.
+    At groups=2, the operation becomes equivalent to having two conv layers side by side, each 
+    seeing half the input channels and producing half the output channels, and both subsequently 
+    concatenated.
+    At groups= in_channels, each input channel is convolved with its own set of filters 
+    (of size (out_channels/in_channels))
 
-  group parameter in Conv2d, splits the output_channel by cardinality.
+    group parameter in Conv2d, splits the output_channel by cardinality.
 
-  """
+    """
   
-  def __init__(self, in_planes, cardinality=32, bottleneck_width=4, stride=1):
-      super(Block, self).__init__()
-      group_width = cardinality * bottleneck_width
-      self.conv1 = nn.Conv2d(in_planes, group_width, kernel_size=1, bias=False)
-      self.bn1 = nn.BatchNorm2d(group_width)
-      """group=cardinality, it divides the out_channel by 32(cardinality) i.e. thus, divides channel 128 into 4"""
-      self.conv2 = nn.Conv2d(group_width, group_width, kernel_size=3, stride=stride, padding=1, groups=cardinality, bias=False)
-      self.bn2 = nn.BatchNorm2d(group_width)
-      self.conv3 = nn.Conv2d(group_width, self.expansion*group_width, kernel_size=1, bias=False)
-      self.bn3 = nn.BatchNorm2d(self.expansion*group_width)
+    def __init__(self, in_planes, cardinality=32, bottleneck_width=4, stride=1):
+        super(Block, self).__init__()
+        group_width = cardinality * bottleneck_width
+        self.conv1 = nn.Conv2d(in_planes, group_width, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(group_width)
+        """group=cardinality, it divides the out_channel by 32(cardinality) i.e. thus, divides channel 128 into 4"""
+        self.conv2 = nn.Conv2d(group_width, group_width, kernel_size=3, stride=stride, padding=1, groups=cardinality, bias=False)
+        self.bn2 = nn.BatchNorm2d(group_width)
+        self.conv3 = nn.Conv2d(group_width, self.expansion*group_width, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*group_width)
 
-      self.shortcut = nn.Sequential()
-      if stride != 1 or in_planes != self.expansion*group_width:
-          self.shortcut = nn.Sequential(
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*group_width:
+            self.shortcut = nn.Sequential(
               nn.Conv2d(in_planes, self.expansion*group_width, kernel_size=1, stride=stride, bias=False),
               nn.BatchNorm2d(self.expansion*group_width)
           )
 
-  def forward(self, x):
-      out = F.relu(self.bn1(self.conv1(x)))
-      out = F.relu(self.bn2(self.conv2(out)))
-      out = self.bn3(self.conv3(out))
-      out += self.shortcut(x)
-      out = F.relu(out)
-      return out
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
 
 class ResNeXt(nn.Module):
