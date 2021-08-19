0a7b369bf41814536f10330c99e2baaba8b812bc
Mayurji <mayur87545@gmail.com>
2021-08-19 20:08:01 +0530

    Learning rates changed

--- augmentations.py
+++ augmentations.py
@@ -3,9 +3,9 @@ import torchvision.transforms as transforms
 
 def augmentation(image_resolution):
     transform = transforms.Compose([transforms.ToTensor(), 
-                transforms.Resize((image_resolution, image_resolution)),
+                transforms.Resize((256, 256)),
                 transforms.ColorJitter(brightness=.5, hue=.3),
-                transforms.RandomCrop(size=(224, 224)),
+                transforms.RandomCrop(size=(image_resolution, image_resolution)),
                 transforms.RandomHorizontalFlip(), 
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
     return transform
@@ -16,8 +16,8 @@ def ContrastiveAugmentation(image_resolution):
     blur = transforms.GaussianBlur((3, 3), (0.1, 2.0))
     transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Resize((image_resolution, image_resolution)),
-    transforms.RandomResizedCrop(size=(224, 224)),
+    transforms.Resize((256, 256)),
+    transforms.RandomResizedCrop(size=(image_resolution, image_resolution)),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomApply([color_jitter], p=0.8),
     transforms.RandomApply([blur], p=0.5),
